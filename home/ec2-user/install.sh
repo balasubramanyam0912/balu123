5d0d3ee209d5d12fb8f4c16a5010e2b724429662
root <root@master.example.com>
2020-02-04 06:15:33 +0000

    second change

--- home/ec2-user/install.sh
+++ home/ec2-user/install.sh
@@ -3,3 +3,8 @@ sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo systemctl status httpd
+sudo yum install ubuntu -y
+sudo systemctl start ubuntu
+sudo systemctl enable ubuntu
+sudo systemctl status ubuntu
+
